f468b5dae2eca95ce557736d9ef7c986f1642089
Mikhail Serov <mikhailserov3@gmail.com>
2018-01-23 10:49:49 +0700

    add greet

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import greet from '..';
+
 console.log('Welcome to the Brain Games!');
+
+greet();
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const greet = () => {
+  console.log('May I have your name?');
+
+  const name = readlineSync.question('Your answer: ');
+  
+  console.log(`Hello, ${name}!`);
+};
+
+export default greet;
