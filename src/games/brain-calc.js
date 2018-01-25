23f0652c37ead0b29b12ef3b02d22a30eec94261
Mikhail Serov <mikhailserov3@gmail.com>
2018-01-25 20:37:27 +0700

    release 3.1.17

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,31 +1,18 @@
-import { cons } from 'hexlet-pairs';
+import { cons, car, cdr } from 'hexlet-pairs';
 import interfaceAll from '..';
 
 const game = () => {
   const rules = 'What is the result of the expression?';
 
   const generateQuestionAnswer = () => {
-    const sign = (index) => {
-      switch (index) {
-        case 0:
-          return '+';
-        case 1:
-          return '-';
-        case 2:
-          return '*';
-        default:
-          return 'default';
-      }
-    };
-
     const compute = (num1, num2, operation) => {
       switch (operation) {
         case 0:
-          return String(num1 + num2);
+          return cons('+', String(num1 + num2));
         case 1:
-          return String(num1 - num2);
+          return cons('-', String(num1 - num2));
         case 2:
-          return String(num1 * num2);
+          return cons('*', String(num1 * num2));
         default:
           return 'default';
       }
@@ -37,8 +24,10 @@ const game = () => {
     const num2 = Math.floor(Math.random() * maxNum);
     const operation = Math.floor(Math.random() * 3);
 
-    const question = `${num1} ${sign(operation)} ${num2}`;
-    const correctAnswer = compute(num1, num2, operation);
+    const signAnswer = compute(num1, num2, operation);
+
+    const question = `${num1} ${car(signAnswer)} ${num2}`;
+    const correctAnswer = cdr(signAnswer);
 
     return cons(question, correctAnswer);
   };
