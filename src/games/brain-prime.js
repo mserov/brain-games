cbee46b1dea352f922513200781f1255cfcc7128
Mikhail Serov <mikhailserov3@gmail.com>
2018-01-28 17:33:36 +0700

    release 7.0.2

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -7,16 +7,16 @@ const game = () => {
   const generateQuestionAnswer = () => {
     const isPrime = (num) => {
       for (let i = 2; i <= num / 2; i += 1) {
-        if (num % i === 0) return 'no';
+        if (num % i === 0) return false;
       }
 
-      return 'yes';
+      return true;
     };
 
     const maxNum = 99;
 
     const question = Math.floor(Math.random() * maxNum) + 2;
-    const correctAnswer = isPrime(question);
+    const correctAnswer = isPrime(question) ? 'yes' : 'no';
 
     return cons(question, correctAnswer);
   };
